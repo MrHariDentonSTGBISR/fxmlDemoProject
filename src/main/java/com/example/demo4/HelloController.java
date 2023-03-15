ac2413976ebec77138a966403ac3e3ee9b999cfe
MrHariDentonSTBISR <hari.denton@stgeorge.school.it>
2023-03-15 11:26:42 +0100

    Added next() and previous() methods and fixed FORWARD_ONLY Error

--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -2,8 +2,7 @@ package com.example.demo4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.sql.*;
 
@@ -16,89 +15,12 @@ public class HelloController {
     static final String PASS = "admin";
     static final String QUERY = "SELECT * FROM actor";
 
-    @FXML
-    private Label calculatorDisplay;
-    private double operand1 = 0;
-    private double operand2 = 0;
-    private double operand3 = 0;
-    private String operator = "";
+    static ResultSet rs;
 
-    private boolean clear = false;
-
-    @FXML
-    protected void buttonEventHandler(ActionEvent event){
+    @FXML TextField txtID;
 
 
-        Button clickedButton = (Button) event.getSource() ;
-        String data = (String) clickedButton.getUserData();
-        System.out.println(data);
-        String text = calculatorDisplay.getText();
 
-        if( text.equals("0") || clear == true){
-            calculatorDisplay.setText(data);
-            clear = false;
-        }
-        else {
-            text = text + data;
-            calculatorDisplay.setText(text);
-            clear = false;
-        }
-    }
-    @FXML
-    protected void operatorEventHandler(ActionEvent event){
-        Button clickedButton = (Button) event.getSource() ;
-        String data = (String) clickedButton.getUserData();
-        String text = calculatorDisplay.getText();
-        // DEAL WITH DECIMAL POINTS
-        if (data.equals(".") && !text.contains(".")) {
-            calculatorDisplay.setText(text + ".");
-        }
-        // DEAL WITH NEGATIVE/POSITIVE NUMBERS
-        if (data.equals("-/+") && !text.contains("-")) {
-            calculatorDisplay.setText("-"+text);
-        }
-        if (data.equals("-/+") && text.contains("-")) {
-            calculatorDisplay.setText(text.substring(1));
-        }
-        // DEAL WITH Clear or Clear Everything
-        if (data.equals("C")) {
-            calculatorDisplay.setText("0");
-        }
-        if (data.equals("CE")) {
-            calculatorDisplay.setText("0");
-            operand1 = 0;
-            operand2 = 0;
-            operand3 = 0;
-            operator = "";
-        }
-        if (operator.equals("") && (data.equals("+") || data.equals("-") || data.equals("*")  || data.equals("/"))  ){
-            operator = data;
-            operand1 = Double.parseDouble(calculatorDisplay.getText());
-            calculatorDisplay.setText("0");
-
-        } else if (data.equals("+") || data.equals("-") || data.equals("*")  || data.equals("/") || data.equals("=") ) {
-            operand2 = Double.parseDouble(calculatorDisplay.getText());
-
-            if (operator.equals("+")){
-                operand3 = operand1 + operand2;
-            }
-            if (operator.equals("-")){
-                operand3 = operand1 - operand2;
-            }
-            if (operator.equals("*")){
-                operand3 = operand1 * operand2;
-            }
-            if (operator.equals("/")){
-                operand3 = operand1 / operand2;
-            }
-            calculatorDisplay.setText(Double.toString(operand3));
-            operand1 = operand3;
-            operator = data;
-            clear = true;
-
-        }
-
-    }
     @FXML
     protected void connectToDB(ActionEvent event){
 
@@ -106,25 +28,39 @@ public class HelloController {
         try{Connection conn = null;
             Class.forName(driverName).newInstance();
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(QUERY);
-
-            while(rs.next()){
-                //Display values
-                System.out.print("ID: " + rs.getInt("actor_id"));
-                System.out.print(", FirstName: " + rs.getString("first_name"));
-                System.out.print(", LasstName: " + rs.getString("last_name"));
-                System.out.println(", LastUpdate: " + rs.getString("last_update"));
-            }
-        } catch (SQLException e) {
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            rs = stmt.executeQuery(QUERY);
+            rs.next();
+            txtID.setText(rs.getString("actor_id"));
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
         }
 
+    }
+
+    @FXML
+    protected void next(ActionEvent event){
+        // write code later
+        // Open a connection
+        try{
+            rs.next();
+            txtID.setText(rs.getString("actor_id"));
+            //System.out.println(rs.getString("actor_id") );
+        } catch (Exception e) {
+            e.printStackTrace();
+            //previous(event);
+        }
+    }
+    @FXML
+    protected void previous(ActionEvent event){
+        // write code later
+        try{
+            rs.previous();
+            txtID.setText(rs.getString("actor_id"));
+            //System.out.println(rs.getString("actor_id") );
+        } catch (Exception e) {
+            e.printStackTrace();
+            //previous(event);
+        }
     }
 }
