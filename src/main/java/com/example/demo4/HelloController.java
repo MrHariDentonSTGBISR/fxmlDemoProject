05c2ccdc501656c60b9b0cf7cd9cac5c56cee111
MrHariDentonSTBISR <hari.denton@stgeorge.school.it>
2023-03-28 15:05:28 +0200

    Added initialise() method to controller and extra fields

--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.sql.*;
@@ -15,29 +16,44 @@ public class HelloController {
     static final String PASS = "admin";
     static final String QUERY = "SELECT * FROM actor";
 
+    static Connection conn = null;
+
     static ResultSet rs;
+    static boolean currentlyEditing = false;
 
     @FXML TextField txtID;
     @FXML TextField txtFirstName;
+    @FXML TextField txtLastName;
+    @FXML TextField txtLastUpdate;
+    @FXML Button btnEdit;
+
 
 
 
     @FXML
-    protected void connectToDB(ActionEvent event){
+    public void initialize(){
 
         // Open a connection
-        try{Connection conn = null;
+        try{
             Class.forName(driverName).newInstance();
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
             rs = stmt.executeQuery(QUERY);
             rs.next();
-            txtID.setText(rs.getString("actor_id"));
-            txtFirstName.setText(rs.getString("first_name"));
+            updateFieldsInView();
+            setEditable(false);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void setEditable(boolean editable) {
+        txtID.setEditable(editable);
+        txtFirstName.setEditable(editable);
+        txtLastName.setEditable(editable);
+        txtLastUpdate.setEditable(editable);
     }
 
     @FXML
@@ -46,9 +62,7 @@ public class HelloController {
         // Open a connection
         try{
             rs.next();
-            txtID.setText(rs.getString("actor_id"));
-            txtFirstName.setText(rs.getString("first_name"));
-            //System.out.println(rs.getString("actor_id") );
+            updateFieldsInView();
         } catch (Exception e) {
             e.printStackTrace();
             //previous(event);
@@ -59,12 +73,47 @@ public class HelloController {
         // write code later
         try{
             rs.previous();
-            txtID.setText(rs.getString("actor_id"));
-            txtFirstName.setText(rs.getString("first_name"));
-            //System.out.println(rs.getString("actor_id") );
+            updateFieldsInView();
         } catch (Exception e) {
             e.printStackTrace();
             //previous(event);
         }
+    }
+
+    protected void updateFieldsInView(){
+        try{
+            txtID.setText(rs.getString("actor_id"));
+            txtFirstName.setText(rs.getString("first_name"));
+            txtLastName.setText(rs.getString("last_name"));
+            txtLastUpdate.setText(rs.getString("last_update"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML protected void update()
+    {
+        if (!currentlyEditing){
+            setEditable(true);
+            currentlyEditing = true;
+            btnEdit.setText("Save");
+        }
+        else {
+            try{
+                rs.updateString("first_name", txtFirstName.getText());
+                rs.updateString("last_name", txtLastName.getText());
+                rs.updateTimestamp("last_update", Timestamp.valueOf(txtLastUpdate.getText()));
+                rs.updateRow();
+
+                setEditable(false);
+                currentlyEditing = false;
+                btnEdit.setText("Edit");
+                updateFieldsInView();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
