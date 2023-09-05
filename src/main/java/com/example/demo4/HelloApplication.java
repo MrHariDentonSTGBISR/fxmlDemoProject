5a642e8117bfe9de762c9870bf05af045b85bf30
Hari Denton <hari.denton@sgbis.it>
2023-09-05 10:43:13 +0200

    Full Update of all db features and the resetable tale

--- src/main/java/com/example/demo4/HelloApplication.java
+++ src/main/java/com/example/demo4/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("FXML Calculator");
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 500);
+        stage.setTitle("FXML Demo Of Loads Of Stuff");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo4/HelloController.java
+++ src/main/java/com/example/demo4/HelloController.java
@@ -2,10 +2,25 @@ package com.example.demo4;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-
 import java.sql.*;
+import java.time.LocalDateTime;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import java.util.Optional;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.util.Callback;
 
 
 public class HelloController {
@@ -20,21 +35,38 @@ public class HelloController {
 
     static ResultSet rs;
     static boolean currentlyEditing = false;
+    static boolean currentlyInserting = false;
 
-    @FXML TextField txtID;
-    @FXML TextField txtFirstName;
-    @FXML TextField txtLastName;
-    @FXML TextField txtLastUpdate;
-    @FXML Button btnEdit;
-
-
-
+    //TABLE VIEW AND DATA
+    private static ObservableList<ObservableList<String>> data;
+    @FXML Button btnSetTableActor;
+    @FXML Button btnSetTableFilm;
 
     @FXML
-    public void initialize(){
+    TableView tableview;
 
+    @FXML
+    TextField txtID;
+    @FXML
+    TextField txtFirstName;
+    @FXML
+    TextField txtLastName;
+    @FXML
+    TextField txtLastUpdate;
+    @FXML
+    Button btnEdit;
+    @FXML
+    Button btnNew;
+    @FXML
+    Button btnDelete;
+    @FXML
+    Button btnLast;
+
+    @FXML
+    public void initialize() {
+        data = FXCollections.observableArrayList();
         // Open a connection
-        try{
+        try {
             Class.forName(driverName).newInstance();
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
@@ -57,49 +89,67 @@ public class HelloController {
     }
 
     @FXML
-    protected void next(ActionEvent event){
-        // write code later
-        // Open a connection
-        try{
-            rs.next();
-            updateFieldsInView();
-        } catch (Exception e) {
-            e.printStackTrace();
-            //previous(event);
+    protected void next(ActionEvent event) {
+        try {
+            if (!rs.next()){rs.first();}
         }
-    }
-    @FXML
-    protected void previous(ActionEvent event){
-        // write code later
-        try{
-            rs.previous();
+        catch (Exception e) {
+            try{
+                rs.first();
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        finally {
             updateFieldsInView();
-        } catch (Exception e) {
-            e.printStackTrace();
-            //previous(event);
         }
     }
 
-    protected void updateFieldsInView(){
-        try{
+    @FXML
+    protected void previous(ActionEvent event) {
+        // write code later
+        try {
+           if (! rs.previous()){rs.last();}
+        }
+        catch (Exception e) {
+            try{
+                System.out.println("2");
+                rs.first();
+                System.out.println("3");
+                rs.last();
+                System.out.println("4");
+            }
+            catch (Exception ex) {
+                System.out.println("5");
+                ex.printStackTrace();
+            }
+        }
+        finally {
+            updateFieldsInView();
+        }
+    }
+
+    protected void updateFieldsInView() {
+        try {
             txtID.setText(rs.getString("actor_id"));
             txtFirstName.setText(rs.getString("first_name"));
             txtLastName.setText(rs.getString("last_name"));
             txtLastUpdate.setText(rs.getString("last_update"));
         } catch (Exception e) {
+            System.out.println("6");
             e.printStackTrace();
         }
     }
 
-    @FXML protected void update()
-    {
-        if (!currentlyEditing){
+    @FXML
+    protected void update() {
+        if (!currentlyEditing) {
             setEditable(true);
             currentlyEditing = true;
             btnEdit.setText("Save");
-        }
-        else {
-            try{
+        } else {
+            try {
                 rs.updateString("first_name", txtFirstName.getText());
                 rs.updateString("last_name", txtLastName.getText());
                 rs.updateTimestamp("last_update", Timestamp.valueOf(txtLastUpdate.getText()));
@@ -109,11 +159,123 @@ public class HelloController {
                 currentlyEditing = false;
                 btnEdit.setText("Edit");
                 updateFieldsInView();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
     }
+    public void insert() {
+        if (!currentlyInserting) {
+            setEditable(true);
+            txtID.setEditable(false);
+            txtID.clear();
+            txtFirstName.clear();
+            txtLastName.clear();
+            txtLastUpdate.clear();
+            currentlyInserting = true;
+            btnNew.setText("Save");
+        } else { try {
+                rs.moveToInsertRow();
+                rs.updateString("first_name", txtFirstName.getText());
+                rs.updateString("last_name", txtLastName.getText());
+                rs.updateTimestamp("last_update", Timestamp.valueOf(LocalDateTime.now()));
+                rs.insertRow();
+                setEditable(false);
+                currentlyEditing = false;
+                btnNew.setText("New");
+                updateFieldsInView();
+            } catch (Exception e) { e.printStackTrace();}
+        }
+    }
+
+    @FXML public void delete(ActionEvent actionEvent) {
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Attention: Action cannot be undone.");
+        alert.setHeaderText("Are you sure you would like to delete this and all related records?");
+        alert.setContentText("Click OK to DELETE or CANCEL to return to the main app.");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            try {
+                //Delete the child references to this actor first
+                String delete_sql = "DELETE FROM film_actor WHERE actor_id="+txtID.getText();
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(delete_sql);
+                //once the references to this actor are deleted we can delete the actor record
+                rs.deleteRow();
+                // call initilize to refresh the resultSet and show the first record
+                initialize();
+                System.out.println("RECORD DELETED");
+            } catch (Exception e)
+            {
+                System.out.println("ERROR IN DELETING");
+                e.printStackTrace();
+            }
+        } else {
+            // ... user chose CANCEL or closed the dialog
+            System.out.println("DELETE CANCELLED BY USER");
+        }
+
+    }//end of method delete
+
+    @FXML public void setTableActor(){
+        String SQLActor = "SELECT * FROM actor";
+        creatTableViewStructure(SQLActor);
+        buildData(SQLActor);
+    }
+
+    @FXML public void setTableFilm(){
+        String SQLFilm = "SELECT * FROM film LIMIT 10";
+        creatTableViewStructure(SQLFilm);
+        buildData(SQLFilm);
+    }
+    @FXML public void buildData(String SQL) {
+        data.clear();
+        try {
+            ResultSet rs = conn.createStatement().executeQuery(SQL);
+            while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+            }
+            tableview.setItems(data);
+            tableview.refresh();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error on Building Data");
+        }
+    }// end buildData
+
+    @FXML public void creatTableViewStructure(String SQL) {
+        tableview.getColumns().clear();
+        try {
+            //ResultSet
+            rs = conn.createStatement().executeQuery(SQL);
+            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
+                        if (param.getValue().get(j)==null){ return new SimpleStringProperty("");}
+                        else { return new SimpleStringProperty(param.getValue().get(j).toString());}
+                    }
+                });
+                tableview.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error on Building Data");
+        }
+    }// end creatTableViewStructure
+
 }
